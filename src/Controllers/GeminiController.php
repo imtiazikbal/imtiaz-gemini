252a6d20a0d370a68362e8ec072f91ce257f10ed
imtiazikbal <imtiaz.ikbal.com@gmail.com>
2024-12-29 12:09:55 +0600

    c

--- src/Controllers/GeminiController.php
+++ src/Controllers/GeminiController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Http;
 use Imtiaz\LaravelGemini\Gemini\GeminiApi;
+use Imtiaz\LaravelGemini\Gemini\MultiPdfUpload;
 use App\Models\Chat;
 
 
